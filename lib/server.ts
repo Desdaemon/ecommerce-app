b520fc3884c0cd2bc8b250551f8a5a4cc2462d95
Viet Dinh <54ckb0y789@gmail.com>
2022-04-20 12:41:40 -0400

    Test using global scope

--- lib/server.ts
+++ lib/server.ts
@@ -35,9 +35,10 @@ function prepareDatabase(path = 'test.db'): Database.Database {
 }
 
 function prepareProductionDatabase(path = 'database.db') {
+  if (global.db_) return global.db_;
   const dbpath = process.env.DB || path;
   const exists = dbpath !== ':memory:' && existsSync(dbpath);
-  const db = Database(dbpath);
+  const db = (global.db_ = Database(dbpath));
   if (!exists) {
     db.exec(schemaFile);
     db.exec(mockFile);
