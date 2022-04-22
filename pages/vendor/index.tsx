d2745c50f3be189d8b2f5fcc0a6d96bfc5ea7e8d
Desdaemon <54ckb0y789@gmail.com>
2022-04-22 12:55:07 -0400

    Vendor Listing (todo)

--- pages/vendor/index.tsx
+++ pages/vendor/index.tsx
@@ -1,6 +1,20 @@
+import RichTextEditor from '@/components/RichTextEditor';
 import VendorLayout from '@/components/VendorLayout';
 import db, { secureSession } from '@/lib/server';
-import { ActionIcon, Button, Group, Table } from '@mantine/core';
+import {
+  ActionIcon,
+  Button,
+  Card,
+  Group,
+  Input,
+  Modal,
+  Table,
+  Text,
+  TextInput,
+} from '@mantine/core';
+import { useForm } from '@mantine/hooks';
+import { AnyTxtRecord } from 'dns';
+import { useEffect, useState } from 'react';
 import { Edit, Trash } from 'tabler-icons-react';
 
 interface VendorListingProps {
@@ -16,6 +30,8 @@ interface VendorListing {
     // img: { url: string }[];
   };
 }
+
+type Listing = VendorListing['listing'];
 
 const getVendorListings = (vendorId: string) =>
   db
@@ -46,11 +62,62 @@ export const getServerSideProps = secureSession<VendorListingProps>(async ({ req
   };
 });
 
+function VendorDialog(props: {
+  onClose(): void;
+  onSubmit(value: Partial<Listing>): any;
+  initialValue?: Partial<Listing>;
+}) {
+  const form = useForm({
+    initialValues: props.initialValue || {},
+    validationRules: {
+      name: Boolean,
+      price: Boolean,
+    },
+    errorMessages: {
+      name: 'Name is required',
+      price: 'Price is required',
+    },
+  });
+  useEffect(() => {
+    if (props.initialValue) form.setValues(props.initialValue);
+  }, [props.initialValue]);
+  return (
+    <Modal
+      title={(props.initialValue?.id ? 'Edit' : 'Add') + ' listing'}
+      opened={!!props.initialValue}
+      onClose={props.onClose}
+    >
+      <form onSubmit={form.onSubmit(props.onSubmit)}>
+        <TextInput label="Name" {...form.getInputProps('name')} />
+        <TextInput label="Price" {...form.getInputProps('price')} />
+        <TextInput label="Description" {...form.getInputProps('description')} />
+        <Group sx={{ paddingTop: 12 }}>
+          <Button type="submit">{props.initialValue?.id ? 'Edit' : 'Add'}</Button>
+          <Button color="red" onClick={props.onClose}>
+            Cancel
+          </Button>
+        </Group>
+      </form>
+    </Modal>
+  );
+}
+
 export default function VendorListings({ items }: VendorListingProps) {
+  const [row, setRow] = useState<Partial<Listing>>();
+  const onClose = () => setRow(undefined);
+  async function onSubmit(value: Listing) {
+    if (row?.id) {
+      console.log({ action: 'edit', value });
+    } else {
+      console.log({ action: 'add', value });
+    }
+    setRow(undefined);
+  }
   return (
     <VendorLayout>
+      <VendorDialog initialValue={row} onClose={onClose} onSubmit={onSubmit} />
       <Group>
-        <Button>Add listing</Button>
+        <Button onClick={() => setRow({})}>Add listing</Button>
       </Group>
       <Table striped highlightOnHover>
         <thead>
@@ -69,7 +136,7 @@ export default function VendorListings({ items }: VendorListingProps) {
               <td style={{ maxWidth: 700 }}>{listing.description}</td>
               <td>
                 <Group>
-                  <ActionIcon>
+                  <ActionIcon onClick={() => setRow({ ...listing })}>
                     <Edit size={16} />
                   </ActionIcon>
                   <ActionIcon>
